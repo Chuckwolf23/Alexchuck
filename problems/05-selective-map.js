c84ea1622867a1d95061c8f6e7e53d8cb676ee0c
Alexander <allanosinhb@gmail.com>
2023-09-11 21:14:38 -0700

    problem 5

--- problems/05-selective-map.js
+++ problems/05-selective-map.js
@@ -37,9 +37,50 @@ console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isPositive, square));
 *******************************************************************************/
 
 function selectiveMap(array, selector, mapper) {
-    // Your code here 
+    // selector and mapper are 2 cbs
+    // create var for newarr
+    let newArr = []
+    // iterate through array
+    for (let i = 0; i < array.length; i++) {
+    // create var for array index
+    let el = array[i]
+    // create if statement 'if first cb === true push to 2nd cb'
+    if (selector(el) === true) {
+        newArr.push(mapper(el))
+    }
+    // create else statement 'first cb === false remains unchanged'
+    else if (selector(el) === false) {
+        newArr.push(el)
+    }
+    }
+
+    // return newarr
+    return newArr
+}
+function isEven(n) {
+    return n % 2 === 0;
 }
 
+function isPositive(n) {
+    return n > 0;
+}
+
+function square(n) {
+    return n * n;
+}
+
+function flipSign(n) {
+    return n * -1;
+}
+
+console.log(selectiveMap([8, 5, 10, 4], isEven, square));
+// [ 64, 5, 100, 16 ]
+
+console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isEven, flipSign));
+// [ 10, -4, 7, -6, 2, -9 ]
+
+console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isPositive, square));
+// [-10, 16, 49, 36, -2, -9]
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
     module.exports = selectiveMap;
