103e975bc6c13fd38294840f08c0f893ead3470c
Chuckwolf23 <championc17@icloud.com>
2023-09-11 23:04:11 -0500

    prob 4 down

--- problems/04-my-filter.js
+++ problems/04-my-filter.js
@@ -20,9 +20,27 @@ console.log(result2);      // ['choose', 'words', 'only']
 *******************************************************************************/
 
 function myFilter(array, cb) {
-    // Your code here 
+    //variable for newarr empty
+    let newarr = []
+    //create for loop
+    for(let i = 0; i < array.length; i++){
+    //create if statement in the loop "if call back is === true "{push call back to newarr}
+    if(cb(array[i])=== true){
+        newarr.push(array[i])
+    }
 }
+    //return newarr
+    return newarr
+}
+let result1 = myFilter([5, 7, 4, 3, 8], function (n) {
+    return n % 2 === 0;
+});
+console.log(result1);       // [ 4, 8 ]
 
+let result2 = myFilter(['choose', 'big', 'words', 'only'], function (s) {
+    return s.length > 3;
+});
+console.log(result2);      // ['choose', 'words', 'only']
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
     module.exports = myFilter;
